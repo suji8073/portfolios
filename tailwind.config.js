7ae140ebee7bc5e999426ebad55702e47f6e1502
suji8073 <suzy8073@naver.com>
2024-11-28 00:28:54 +0900

    chore: tailwind font 추가 - RixInooAriDuriR, GmarketSansMedium 웹 폰트 추가 - 해당 폰트 없을 경우 Noto SansKR 폰트로 지정되도록 함

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rix: ['RixInooAriDuriR', 'Noto Sans KR', 'Arial', 'sans-serif'],
+        gmk: ['GmarketSansMedium', 'Noto Sans KR', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
