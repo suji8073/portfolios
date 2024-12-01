c95da1ae03c4f8db032667769cfd97b4e44d9f6d
suji8073 <suzy8073@naver.com>
2024-12-01 16:14:10 +0900

    feat: React 라우터를 사용한 라우팅 및 페이지 구성 - Home, Profile, Project 정보를 담을 파일 생성 - react-router-dom을 사용한 라우팅 기능 개발

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './page/Home';
+import Profile from './page/Profile';
+import Project from './page/Project';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/PROFILE' element={<Profile />} />
+        <Route path='/PROJECT' element={<Project />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
