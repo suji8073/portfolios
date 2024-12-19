48dd3f28707aa485d384008b380c1ae2f35094d8
suji8073 <suzy8073@naver.com>
2024-12-20 00:41:30 +0900

    [feat] Header 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
 import Profile from './page/Profile';
 import Project from './page/Project';
+import Header from './components/common/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/PROFILE' element={<Profile />} />
